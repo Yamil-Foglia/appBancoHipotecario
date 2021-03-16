aab199493b1bd8e01b7675578429940ee7897c6e
Yamil Foglia <yamil.foglia@outlook.com>
2021-03-16 17:08:52 -0300

    redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/shared/login/login.component';
+import { AssignmentComponent } from './views/student/assignment/assignment.component';
+import { DashboardComponent } from './views/teacher/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'student/assignment', component: AssignmentComponent},
+  {path: 'teacher/dashboard', component: DashboardComponent},
+
 ];
 
 @NgModule({
